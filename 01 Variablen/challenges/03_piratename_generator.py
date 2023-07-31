0a5a66e163f9b138ba53c934fa9a097977741962
reinhardt <s_reinhardt@outlook.de>
2023-07-31 10:16:03 +0200

    Update 03_piratename_generator.py
    added solution

--- 01 Variablen/challenges/03_piratename_generator.py
+++ 01 Variablen/challenges/03_piratename_generator.py
@@ -1,12 +1,12 @@
 # Erstelle eine Begrüßung
-
+print('Ayeeee Mate, du bist also der neue Käpt\'n 🏴‍☠️'
 # Frage die Nutzer*in nach der Farbe des Lieblingstiers
-
+color = input('Welche Farbe hat dein Lieblingstier:\n')
 # Frage die Nutzer*in nach einem Körper- oder Schiffsteil
-
+part = input('Mit welchem Schiffsteil findest du dich am ehesten verbunden:\n')
 # Kombiniere das Wort "Käpt'n" mit der Farbe und dem und dem Körper- bzw Schiffsteil zu einem Piratennamen und
 # speichere diesen in einer Variable
-
+name = f'Käpt\'n {color} {part}'
 # Zeige den Piratennamen an
-
+print(f'Aye, dann bist du also {name}')
 # Alle Nutzereingaben sollen in einer neuen Zeile und nicht direkt hinter der Frage erfolgen
